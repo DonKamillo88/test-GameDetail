91df61142c26c042da9afa02dcb362ff7cbd4798
Kamil Popławski <kamil.poplawski88@gmail.com>
2017-06-28 04:19:33 +0100

    show service logs

--- app/src/main/java/com/donkamillo/gamedetails/data/remote/DropBoxService.java
+++ app/src/main/java/com/donkamillo/gamedetails/data/remote/DropBoxService.java
@@ -2,6 +2,7 @@ package com.donkamillo.gamedetails.data.remote;
 
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -14,11 +15,15 @@ public class DropBoxService {
     public static String API_URL = "https://www.dl.dropboxusercontent.com";
 
     public static DropBox getService() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
 
         return new Retrofit.Builder()
                 .baseUrl(API_URL)
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build()
                 .create(DropBox.class);
     }
